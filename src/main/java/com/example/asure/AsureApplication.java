0007445658c96771ba498a3159a0e2c914aedbf4
ITC-NINHTRAN-TB\ninh.tran <ninh.tran@mservice.com.vn>
2022-07-12 19:46:26 +0700

    init prj

---
+++ src/main/java/com/example/asure/AsureApplication.java
@@ -0,0 +1,13 @@
+package com.example.asure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asure/AsureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
